e83bc1b1928e8bf6a96e26f7f40972e03571ca8f
hamdalla2 <hamdalla2@yahoo.com>
2020-11-02 15:47:35 +0200

    H

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
-
-
+let c=function(x){
+if(x.innerText==="A"){x.innerText="X";x.style.color="red"}
+else if(x.innerText==="X"){x.innerText="O";x.style.color="red"}
+else{x.innerText="A";x.style.color="pink"}
+}
