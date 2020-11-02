ce3f0167f2b869ce9e1a99a3eb2e87d2e262c542
hamdalla2 <hamdalla2@yahoo.com>
2020-11-02 15:55:14 +0200

    H

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let c=function(x){
 if(x.innerText==="N"){x.innerText="X";x.style.color="red"}
-else if(x.innerText==="X"){x.innerText="O";x.style.color="red"}
+else if(x.innerText==="X"){x.innerText="O";x.style.color="blue"}
 else{x.innerText="N";x.style.color="pink"}
 console.log(x.id +" : "+x.innerText)
 }
